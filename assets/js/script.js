d26eed731f46d290ec9a53eb52eb8fe3e94d88c1
Nick Simoncelli <Nicksimoncelli@gmail.com>
2019-05-22 17:57:17 -0700

    guessing game make 2 cards match- Nick Simoncelli

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,28 @@ $(document).ready(intializeApp);
 
 function intializeApp() {
 
+    var firstCardClicked = null;
+    var secondCardClicked = null;
+    var matches = null;
+
     $('.card').click(handleCardClick);
 
     function handleCardClick(event) {
         $(this).find('.back').addClass('hidden');
+        console.log("this is : ", this)
+        if (firstCardClicked === null) {
+            firstCardClicked = $(this).find('.front').css("background-image");
+        } else {
+            secondCardClicked = $(this).find('.front').css("background-image");
+        }
+        console.log("first card click: ", firstCardClicked);
+        console.log("second card click: ", secondCardClicked);
+
+        if (firstCardClicked === secondCardClicked) {
+            console.log("cards match");
+            matches += 1;
+        } else {
+            console.log("cards do not match");
+        }
     }
 }
