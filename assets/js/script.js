829d0e247e40f728118f8f802bf33bcd4b26dae6
Nick Simoncelli <Nicksimoncelli@gmail.com>
2019-05-24 16:40:17 -0700

    updated into the next feature so i dont lose my work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,93 @@
 $(document).ready(intializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+var max_matches = 9;
+var attempts = 0;
+var games_played = null;
+
 function intializeApp() {
 
-    var firstCardClicked = null;
-    var secondCardClicked = null;
-    var matches = null;
-    var max_matches = 2;
     addModalCloseHandler();
+    displayStats();
 
     $('.card').click(handleCardClick);
+}
 
-    function handleCardClick(event) {
-        $(this).find('.back').addClass('hidden');
-        console.log("this is : ", this)
-        if (firstCardClicked === null) {
-            firstCardClicked = $(this)
-            return;
-        } else {
-            secondCardClicked = $(this)
-        }
-        console.log("first card click: ", firstCardClicked);
-        console.log("second card click: ", secondCardClicked);
-        var firstCardImgSrc = firstCardClicked.find('.front').css("background-image");
-        var secondCardImgSrc = secondCardClicked.find('.front').css("background-image");
-        if (firstCardImgSrc === secondCardImgSrc) {
-            console.log("cards match");
-            matches += 1;
+function handleCardClick(event) {
+    $(this).find('.back').addClass('hidden');
+    console.log("this is : ", this)
+    if (firstCardClicked === null) {
+        firstCardClicked = $(this)
+        return;
+    } else {
+        secondCardClicked = $(this)
+    }
+    console.log("first card click: ", firstCardClicked);
+    console.log("second card click: ", secondCardClicked);
+    var firstCardImgSrc = firstCardClicked.find('.front').css("background-image");
+    var secondCardImgSrc = secondCardClicked.find('.front').css("background-image");
+    if (firstCardImgSrc === secondCardImgSrc) {
+        console.log("cards match");
+        matches += 1;
+        firstCardClicked = null;
+        secondCardClicked = null;
+        attempts += 1;
+    } else {
+        console.log("cards do not match");
+        attempts += 1;
+        setTimeout(function() {
+            $(firstCardClicked).find('.back').removeClass('hidden');
+            $(secondCardClicked).find('.back').removeClass('hidden');
             firstCardClicked = null;
             secondCardClicked = null;
-        } else {
-            console.log("cards do not match");
-            setTimeout(function() {
-                $(firstCardClicked).find('.back').removeClass('hidden');
-                $(secondCardClicked).find('.back').removeClass('hidden');
-                firstCardClicked = null;
-                secondCardClicked = null;
-            }, 1500);
+        }, 1500);
 
-        }
-
-        if (matches === max_matches) {
-            console.log("you win the matching game");
-            myModal();
-        }
     }
 
-
-    function myModal() {
+    if (matches === max_matches) {
+        console.log("you win the matching game");
+        games_played += 1;
+        myModal();
         setTimeout(function() {
-            $('.modal').css('display', 'block');
+            $('.back').removeClass('hidden');
 
         }, 500);
-
     }
+    displayStats();
+}
 
-    function addModalCloseHandler() {
-
-        $("body").click(function() {
-            $(".modal").css('display', 'none');
-        });
+function calculateAccuracy() {
+    var calcAcc = matches / attempts;
+    if (calcAcc === 0 || calcAcc === NaN) {
+        return 0;
+    } else {
+        return calcAcc;
     }
+}
+
+
+function myModal() {
+    setTimeout(function() {
+        $('.modal').css('display', 'block');
+
+    }, 500);
+
+}
+
+function addModalCloseHandler() {
+
+    $("body").click(function() {
+        $(".modal").css('display', 'none');
+    });
+}
+
+function displayStats() {
+    var accuracyResults = calculateAccuracy();
+    $('#accuracy').text(accuracyResults + "%");
+    $('#gamesPlayed').text(games_played);
+    $('#attemptedGames').text(attempts);
+
+
 }
